f1f4821493e8174db6c5fcb5b54fdea3dfa1f870
Satwik Kambham <satwik.kambham@gmail.com>
2023-01-26 19:41:21 +0530

    Updated transform

---
+++ insight/core/trainer.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+
+class Trainer:
+    def __init__(self, model) -> None:
+        pass
+
+    def train_step():
+        pass
+
+    def val_step():
+        pass
+
+    def train_loop():
+        pass
+
+    def val_loop():
+        pass
+
+    def fit():
+        pass
+
+    def save_checkpoint():
+        pass
+
+    def load_checkpoint():
+        pass
