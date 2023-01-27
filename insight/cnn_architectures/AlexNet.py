5e7dd0b2f01cba134914c2c910009af092c4b229
Satwik Kambham <satwik.kambham@gmail.com>
2023-01-27 11:18:48 +0530

    Added flatten to alexnet

--- insight/cnn_architectures/AlexNet.py
+++ insight/cnn_architectures/AlexNet.py
@@ -11,6 +11,7 @@ class AlexNet(nn.Module):
             *[nn.LazyConv2d(i, 3, padding=1) for i in [384, 384, 256]]
         )
         self.pool = nn.MaxPool2d(3, stride=2)
+        self.flatten = nn.Flatten()
         self.fc1 = nn.LazyLinear(4096)
         self.fc2 = nn.LazyLinear(4096)
         self.fc3 = nn.LazyLinear(num_classes)
@@ -20,6 +21,7 @@ class AlexNet(nn.Module):
         out = self.pool(F.relu(self.conv1(x)))
         out = self.pool(F.relu(self.conv2(out)))
         out = self.pool(F.relu(self.conv3(out)))
+        out = self.flatten(out)
         out = self.dropout(F.relu(self.fc1(out)))
         out = self.dropout(F.relu(self.fc2(out)))
         out = self.fc3(out)
