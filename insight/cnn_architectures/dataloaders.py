d27187b1cb734a79022149bafe42f9fee6e6b679
Satwik Kambham <satwik.kambham@gmail.com>
2023-01-27 12:21:01 +0530

    Added Caltech101 and updated transform

--- insight/cnn_architectures/dataloaders.py
+++ insight/cnn_architectures/dataloaders.py
@@ -62,7 +62,29 @@ def load_Caltech256(root):
     # Loading the dataset
     img_transform = transforms.Compose(
         [
-            transforms.CenterCrop(256),
+            transforms.Resize(256),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.repeat(3, 1, 1) if x.shape[0] == 1 else x),
+        ]
+    )
+    label_transform = transforms.Compose([])
+    dataset = datasets.Caltech256(
+        root,
+        transform=img_transform,
+        target_transform=label_transform,
+        download=True,
+    )
+
+    # Splitting the dataset into train and validation
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [0.7, 0.3])
+
+    return train_dataset, val_dataset
+
+def load_Caltech101(root):
+    # Loading the dataset
+    img_transform = transforms.Compose(
+        [
+            transforms.CenterCrop((300, 200)),
             transforms.ToTensor(),
             transforms.Lambda(lambda x: x.repeat(3, 1, 1) if x.shape[0] == 1 else x),
         ]
