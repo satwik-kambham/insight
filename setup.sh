5fdac6f1fbe543efe632b0b97c00be60ac17a998
Satwik Kambham <satwik.kambham@gmail.com>
2023-01-29 12:53:43 +0530

    Wandb login and environment creation

--- setup.sh
+++ setup.sh
@@ -11,12 +11,19 @@ bash Mambaforge-Linux-x86_64.sh -b
 
 rm -f Mambaforge-Linux-x86_64.sh
 
+# Create environment
+~/mambaforge/bin/mamba create -n insight python=3.10 -y
+~/mambaforge/bin/mamba activate insight
+
 # Install dependencies
 ~/mambaforge/bin/mamba install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch-nightly -c nvidia -y
 
 ~/mambaforge/bin/mamba install pytorch-lightning torchinfo wandb -c conda-forge -y
 
 ~/mambaforge/bin/mamba install -c "nvidia/label/cuda-11.7.0" cuda-nvcc -y
+
+# Login to wandb
+wandb login
 
 # Update repository
 cd insight
