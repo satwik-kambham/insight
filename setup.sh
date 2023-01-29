f14df05a9cbc2f6243ef01fa4bed8ee4202d7839
Satwik Kambham <satwik.kambham@gmail.com>
2023-01-29 12:18:53 +0530

    Updated path issue in setup

--- setup.sh
+++ setup.sh
@@ -7,22 +7,22 @@ wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambafor
 
 bash Mambaforge-pypy3-Linux-x86_64.sh -b
 
-~/mambaforge/condabin/conda init
+~/mambaforge-pypy3/condabin/conda init
 
 rm -f Mambaforge-pypy3-Linux-x86_64.sh
 
 # Install dependencies
-/root/mambaforge/bin/mamba install numpy pandas matplotlib seaborn bokeh -y
-mamba install jupyter jupyterlab ipython ipykernel -y
+~/mambaforge-pypy3/bin/mamba install numpy pandas matplotlib seaborn bokeh -y
+~/mambaforge-pypy3/bin/mamba mamba install jupyter jupyterlab ipython ipykernel -y
 
-/root/mambaforge/bin/mamba install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch-nightly -c nvidia -y
+~/mambaforge-pypy3/bin/mamba install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch-nightly -c nvidia -y
 
-/root/mambaforge/bin/mamba install pytorch-lightning -c conda-forge -y
-/root/mambaforge/bin/mamba install torchinfo -c conda-forge -y
+~/mambaforge-pypy3/bin/mamba install pytorch-lightning -c conda-forge -y
+~/mambaforge-pypy3/bin/mamba install torchinfo -c conda-forge -y
 
-/root/mambaforge/bin/mamba install wandb -c conda-forge -y
+~/mambaforge-pypy3/bin/mamba install wandb -c conda-forge -y
 
-/root/mambaforge/bin/mamba install -c "nvidia/label/cuda-11.7.0" cuda-nvcc -y
+~/mambaforge-pypy3/bin/mamba install -c "nvidia/label/cuda-11.7.0" cuda-nvcc -y
 
 # Update repository
 cd insight
