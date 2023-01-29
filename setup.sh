3f9c2ad49667a7905a419198750992b2bab5ad6d
Satwik Kambham <satwik.kambham@gmail.com>
2023-01-29 12:09:52 +0530

    Updating setup.py

--- setup.sh
+++ setup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-# Create and activate environment
-mamba create -n insight python=3.10
-mamba activate insight
+rm -f Mambaforge-pypy3-Linux-x86_64.sh
+
+# Install Mambaforge (mamba)
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-pypy3-Linux-x86_64.sh"
+
+bash Mambaforge-pypy3-Linux-x86_64.sh -b
+
+~/mambaforge/condabin/conda init
+
+rm -f Mambaforge-pypy3-Linux-x86_64.sh
 
 # Install dependencies
-mamba install numpy pandas matplotlib seaborn bokeh -y
+/root/mambaforge/bin/mamba install numpy pandas matplotlib seaborn bokeh -y
 mamba install jupyter jupyterlab ipython ipykernel -y
 
-mamba install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch-nightly -c nvidia -y
+/root/mambaforge/bin/mamba install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch-nightly -c nvidia -y
 
-mamba install pytorch-lightning -c conda-forge -y
-mamba install torchinfo -c conda-forge -y
+/root/mambaforge/bin/mamba install pytorch-lightning -c conda-forge -y
+/root/mambaforge/bin/mamba install torchinfo -c conda-forge -y
 
-mamba install wandb -c conda-forge -y
+/root/mambaforge/bin/mamba install wandb -c conda-forge -y
 
-mamba install -c "nvidia/label/cuda-11.7.0" cuda-nvcc -y
+/root/mambaforge/bin/mamba install -c "nvidia/label/cuda-11.7.0" cuda-nvcc -y
 
 # Update repository
 cd insight
