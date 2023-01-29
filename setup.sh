64df043d83937b67d24462b865a70dadda2b0e33
Satwik Kambham <satwik.kambham@gmail.com>
2023-01-29 10:25:24 +0530

    Updated to not ask for every install

--- setup.sh
+++ setup.sh
@@ -5,17 +5,17 @@ mamba create -n insight python=3.10
 mamba activate insight
 
 # Install dependencies
-mamba install numpy pandas matplotlib seaborn bokeh
-mamba install jupyter jupyterlab ipython ipykernel
+mamba install numpy pandas matplotlib seaborn bokeh -y
+mamba install jupyter jupyterlab ipython ipykernel -y
 
-mamba install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch-nightly -c nvidia
+mamba install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch-nightly -c nvidia -y
 
-mamba install pytorch-lightning -c conda-forge
-mamba install torchinfo -c conda-forge
+mamba install pytorch-lightning -c conda-forge -y
+mamba install torchinfo -c conda-forge -y
 
-mamba install wandb -c conda-forge
+mamba install wandb -c conda-forge -y
 
-conda install -c "nvidia/label/cuda-11.7.0" cuda-nvcc
+mamba install -c "nvidia/label/cuda-11.7.0" cuda-nvcc -y
 
 # Update repository
 cd insight
