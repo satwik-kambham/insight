3a0e8d986bd230c50b586dcc6bbe47e231565fc3
Satwik Kambham <satwik.kambham@gmail.com>
2023-01-29 10:15:44 +0530

    Moved downloading mamba to different script

---
+++ install-mamba.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -f Mambaforge-pypy3-Linux-x86_64.sh
+
+# Install Mambaforge (mamba)
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-pypy3-Linux-x86_64.sh"
+
+bash Mambaforge-pypy3-Linux-x86_64.sh
--- setup.sh
+++ setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-rm -f Mambaforge-pypy3-Linux-x86_64.sh
-
-# Install Mambaforge (mamba)
-wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-pypy3-Linux-x86_64.sh"
-
-bash Mambaforge-pypy3-Linux-x86_64.sh
-
 # Create and activate environment
 mamba create -n insight python=3.10
 mamba activate insight
