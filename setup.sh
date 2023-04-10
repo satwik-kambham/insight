fb094b7db8976ae09631e8ec50d322b1bfdb78dc
Satwik Kambham <satwik.kambham@gmail.com>
2023-04-11 00:41:59 +0530

    Update setup script

--- setup.sh
+++ setup.sh
@@ -6,23 +6,8 @@ rm -f Mambaforge-Linux-x86_64.sh
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh"
 
 bash Mambaforge-Linux-x86_64.sh -b
-
 ~/mambaforge/condabin/conda init
-
 rm -f Mambaforge-Linux-x86_64.sh
 
 # Install dependencies
-~/mambaforge/bin/mamba install pytorch torchvision torchaudio pytorch-cuda=11.6 -c pytorch -c nvidia -y
-~/mambaforge/bin/mamba install networkx -y
-~/mambaforge/bin/mamba install torchmetrics torchinfo wandb -y
-~/mambaforge/bin/mamba install scipy -y
-~/mambaforge/bin/mamba install -c "nvidia/label/cuda-11.7.0" cuda-nvcc -y
-~/mambaforge/bin/mamba install ray-tune optuna -y
-
-~/mambaforge/bin/mamba install ray-tune ipython -y
-
-~/mambaforge/bin/pip install selectivesearch
-~/mambaforge/bin/pip install "pytorch-lightning[extra]"
-
-# Update repository
-git pull
+~/mambaforge/bin/mamba env create --file environment.yml
