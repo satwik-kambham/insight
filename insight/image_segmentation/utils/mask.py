2a82a0b617b91db010ef0a9241488c1858a4a842
Satwik Kambham <satwik.kambham@gmail.com>
2023-05-31 17:26:25 +0530

    Update mask generation

--- insight/image_segmentation/utils/mask.py
+++ insight/image_segmentation/utils/mask.py
@@ -4,17 +4,20 @@ import matplotlib
 from PIL import Image
 
 
-def generate_mask_from_class_probabilities(pred, num_classes):
-    pred_labels = pred.argmax(dim=1, keepdim=False).cpu().numpy()
+def generate_mask(pred, num_classes, one_hot=True):
+    if one_hot:
+        pred_labels = pred.argmax(dim=1, keepdim=False).cpu().numpy()
+    else:
+        pred_labels = pred.cpu().numpy()
 
     # Create color map given number of classes
-    color_map = matplotlib.colormaps.get_cmap("hsv")
+    color_map = matplotlib.colormaps.get_cmap("gnuplot2")
     color_map = matplotlib.colors.ListedColormap(
         color_map(np.linspace(0, 1, num_classes))
     )
 
-    rgb_mask = color_map(pred_labels[0])
-    rgb_mask = (rgb_mask * 255).astype(np.uint8)
+    rgb_mask = color_map(pred_labels[0] / num_classes)
+    rgb_mask = (rgb_mask[:, :, :3] * 255).astype(np.uint8)
     rgb_image = Image.fromarray(rgb_mask)
 
     return rgb_image
