34d73122f933230e86e5a06f29921fe018334715
Satwik Kambham <satwik.kambham@gmail.com>
2023-05-31 17:24:00 +0530

    Update dataloader

--- insight/image_segmentation/data/dataloaders.py
+++ insight/image_segmentation/data/dataloaders.py
@@ -1,48 +1,141 @@
-import torch
 import numpy as np
+
+import torch
+from torch.utils.data import random_split
 from torchvision import datasets, transforms
 
+import albumentations as A
 
-def load_VOCSegmentation(data_dir):
+
+class AugmentedDataset:
+    def __init__(
+        self,
+        dataset,
+        augmentation=None,
+        img_transforms=None,
+        mask_transforms=None,
+    ):
+        self.dataset = dataset
+        self.augmentation = augmentation
+        self.img_transforms = img_transforms
+        self.mask_transforms = mask_transforms
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img, mask = self.dataset[idx]
+
+        img = np.array(img)
+        mask = np.array(mask)
+
+        if self.augmentation:
+            augmented = self.augmentation(image=img, mask=mask)
+            img = augmented["image"]
+            mask = augmented["mask"]
+
+        if self.img_transforms:
+            img = self.img_transforms(img)
+
+        if self.mask_transforms:
+            mask = self.mask_transforms(mask)
+
+        return img, mask
+
+
+def get_augmentation(img_shape):
+    train_transform = A.Compose(
+        [
+            A.Resize(*img_shape),
+            A.HorizontalFlip(),
+            A.RandomBrightnessContrast(),
+        ]
+    )
+
+    val_transform = A.Compose(
+        [
+            A.Resize(*img_shape),
+        ]
+    )
+
+    return train_transform, val_transform
+
+
+def load_OxfordIIITPetDataset(data_dir, img_shape=(128, 128)):
     img_transforms = transforms.Compose(
         [
-            transforms.Resize((572, 572)),
+            transforms.ToTensor(),
+        ]
+    )
+
+    def mask_to_tensor(mask):
+        mask = mask - 1
+        tensor_mask = torch.from_numpy(mask).long()
+        return tensor_mask
+
+    mask_transforms = transforms.Compose(
+        [
+            transforms.Lambda(lambda x: mask_to_tensor(x)),
+        ]
+    )
+
+    dataset = datasets.OxfordIIITPet(
+        root=data_dir,
+        split="trainval",
+        target_types="segmentation",
+        download=True,
+    )
+
+    train_dataset, val_dataset = random_split(dataset, [0.9, 0.1])
+    train_augmentation, val_augmentation = get_augmentation(img_shape)
+    train_dataset = AugmentedDataset(
+        train_dataset, train_augmentation, img_transforms, mask_transforms
+    )
+    val_dataset = AugmentedDataset(
+        val_dataset, val_augmentation, img_transforms, mask_transforms
+    )
+
+    num_classes = 3
+
+    return train_dataset, val_dataset, num_classes
+
+
+def load_VOCSegmentationDataset(data_dir, img_shape=(128, 128)):
+    img_transforms = transforms.Compose(
+        [
             transforms.ToTensor(),
         ]
     )
 
     def mask_to_tensor(mask):
         mask = np.array(mask)
-        mask = mask == 0
+        mask[mask == 255] = 21
         tensor_mask = torch.from_numpy(mask).long()
-        tensor_mask = (
-            torch.nn.functional.one_hot(tensor_mask, num_classes=2)
-            .permute(2, 0, 1)
-            .float()
-        )
         return tensor_mask
 
     mask_transforms = transforms.Compose(
         [
-            transforms.Resize((388, 388)),
             transforms.Lambda(lambda x: mask_to_tensor(x)),
         ]
     )
 
-    train_dataset = datasets.VOCSegmentation(
+    dataset = datasets.VOCSegmentation(
         root=data_dir,
-        image_set="train",
+        image_set="trainval",
         transform=img_transforms,
         target_transform=mask_transforms,
         download=True,
     )
 
-    val_dataset = datasets.VOCSegmentation(
-        root=data_dir,
-        image_set="val",
-        transform=img_transforms,
-        target_transform=mask_transforms,
-        download=True,
+    train_dataset, val_dataset = random_split(dataset, [0.9, 0.1])
+    train_augmentation, val_augmentation = get_augmentation(img_shape)
+    train_dataset = AugmentedDataset(
+        train_dataset, train_augmentation, img_transforms, mask_transforms
+    )
+    val_dataset = AugmentedDataset(
+        val_dataset, val_augmentation, img_transforms, mask_transforms
     )
 
-    return train_dataset, val_dataset
+    num_classes = 22
+
+    return train_dataset, val_dataset, num_classes
--- insight/image_segmentation/data/datamodule.py
+++ insight/image_segmentation/data/datamodule.py
@@ -2,26 +2,37 @@ from torch.utils.data import DataLoader
 
 import lightning.pytorch as pl
 
-from .dataloaders import load_VOCSegmentation
+from .dataloaders import load_OxfordIIITPetDataset, load_VOCSegmentationDataset
 
 
-class VOCSegmentationDataModule(pl.LightningDataModule):
+class SegmentationDataModule(pl.LightningDataModule):
     def __init__(
         self,
         data_dir,
+        dataset="OxfordIIITPet",
         batch_size=1,
         num_workers=0,
     ):
         super().__init__()
 
         self.data_dir = data_dir
+        self.dataset_name = dataset
         self.batch_size = batch_size
         self.num_workers = num_workers
 
         self.save_hyperparameters()
 
     def setup(self, stage):
-        self.train_dataset, self.val_dataset = load_VOCSegmentation(self.data_dir)
+        if self.dataset_name == "OxfordIIITPet":
+            self.train_dataset, self.val_dataset = load_OxfordIIITPetDataset(
+                self.data_dir
+            )
+        elif self.dataset_name == "VOCSegmentation":
+            self.train_dataset, self.val_dataset = load_VOCSegmentationDataset(
+                self.data_dir
+            )
+        else:
+            raise NotImplementedError
 
     def train_dataloader(self):
         return DataLoader(
