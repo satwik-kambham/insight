dc02637fe2d9c4bd3be1d5be9c75e028d31ab0ee
Satwik Kambham <satwik.kambham@gmail.com>
2023-05-31 17:26:16 +0530

    Update model

--- insight/image_segmentation/models/unet.py
+++ insight/image_segmentation/models/unet.py
@@ -8,90 +8,60 @@ import lightning.pytorch as pl
 from lightning.pytorch.loggers import TensorBoardLogger, WandbLogger
 import torchmetrics as tm
 
-from ..utils import mask
+from ..utils.mask import generate_mask
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.LazyConv2d(out_channels, kernel_size=3, padding="same"),
+            nn.ReLU(),
+            nn.LazyBatchNorm2d(),
+            nn.LazyConv2d(out_channels, kernel_size=3, padding="same"),
+            nn.ReLU(),
+            nn.LazyBatchNorm2d(),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
 
 
 class UNet(nn.Module):
     def __init__(self, out_channels):
         super(UNet, self).__init__()
+        self.down1 = DoubleConv(64)
+        self.down2 = DoubleConv(128)
+        self.down3 = DoubleConv(256)
+        self.down4 = DoubleConv(512)
+        self.up1 = nn.LazyConvTranspose2d(512, kernel_size=2, stride=2)
+        self.up2 = nn.LazyConvTranspose2d(256, kernel_size=2, stride=2)
+        self.up3 = nn.LazyConvTranspose2d(128, kernel_size=2, stride=2)
+        self.up4 = DoubleConv(64)
 
-        # Encoder
-        self.encoder_blocks = nn.ModuleList(
-            [
-                self.block(64),
-                self.contracting_block(128),
-                self.contracting_block(256),
-                self.contracting_block(512),
-            ]
-        )
-
-        # Bottleneck
-        self.bottleneck = nn.Sequential(
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            self.expanding_block(1024),
-        )
-
-        # Decoder
-        self.decoder_blocks = nn.ModuleList(
-            [
-                self.expanding_block(512),
-                self.expanding_block(256),
-                self.expanding_block(128),
-                self.block(64),
-            ]
-        )
-
-        # # Output layer
         self.output = nn.LazyConv2d(out_channels, kernel_size=1)
 
-    def block(self, out_channels):
-        return nn.Sequential(
-            nn.LazyConv2d(out_channels, kernel_size=3),
-            nn.ReLU(),
-            nn.LazyBatchNorm2d(),
-            nn.LazyConv2d(out_channels, kernel_size=3),
-            nn.ReLU(),
-            nn.LazyBatchNorm2d(),
-        )
-
-    def contracting_block(self, out_channels):
-        return nn.Sequential(
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            self.block(out_channels),
-        )
-
-    def expanding_block(self, out_channels):
-        return nn.Sequential(
-            self.block(out_channels),
-            nn.LazyConvTranspose2d(out_channels, kernel_size=2, stride=2),
-        )
-
     def forward(self, x):
-        encoder_features = []
+        # Downsampling path
+        x1 = self.down1(x)
+        x2 = nn.functional.max_pool2d(x1, kernel_size=2, stride=2)
+        x3 = self.down2(x2)
+        x4 = nn.functional.max_pool2d(x3, kernel_size=2, stride=2)
+        x5 = self.down3(x4)
+        x6 = nn.functional.max_pool2d(x5, kernel_size=2, stride=2)
+        x7 = self.down4(x6)
 
-        # Encoder
-        for encoder_block in self.encoder_blocks:
-            x = encoder_block(x)
-            encoder_features.append(x)
+        # Upsampling path
+        x = self.up1(x7)
+        x = torch.cat([x, x5], dim=1)
+        x = self.up2(x)
+        x = torch.cat([x, x3], dim=1)
+        x = self.up3(x)
+        x = torch.cat([x, x1], dim=1)
+        x = self.up4(x)
+        x = self.output(x)
 
-        # Bottleneck
-        x = self.bottleneck(x)
-
-        # # Decoder
-        for decoder_block, encoder_feature in zip(
-            self.decoder_blocks, reversed(encoder_features)
-        ):
-            crop = (encoder_feature.size()[2] - x.size()[2]) // 2
-            encoder_feature = encoder_feature[
-                :, :, crop : crop + x.size()[2], crop : crop + x.size()[3]
-            ]
-
-            x = torch.cat([x, encoder_feature], dim=1)
-            x = decoder_block(x)
-
-        # Output
-        output = self.output(x)
-        return output
+        return x
 
 
 class UNetModule(pl.LightningModule):
@@ -99,8 +69,6 @@ class UNetModule(pl.LightningModule):
         self,
         model,
         lr=0.01,
-        momentum=0.99,
-        weight_decay=0.0005,
         num_classes=2,
     ):
         super().__init__()
@@ -110,16 +78,11 @@ class UNetModule(pl.LightningModule):
         self.model = model
 
         self.lr = lr
-        self.momentum = momentum
-        self.weight_decay = weight_decay
         self.num_classes = num_classes
 
-        if num_classes == 2:
-            self.criterion = nn.BCEWithLogitsLoss()
-        else:
-            self.criterion = nn.CrossEntropyLoss()
+        self.criterion = nn.CrossEntropyLoss()
 
-        # self.iou = tm.JaccardIndex(task="multiclass", num_classes=num_classes)
+        self.accuracy = tm.Accuracy(task="multiclass", num_classes=num_classes)
 
     def forward(self, x):
         return self.model(x)
@@ -128,23 +91,23 @@ class UNetModule(pl.LightningModule):
         data, target = batch
         output = self(data)
         loss = self.criterion(output, target)
-        self.log("train_loss", loss)
+        self.log("loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         data, target = batch
         output = self(data)
         loss = self.criterion(output, target)
-        # pred = output.argmax(dim=1, keepdim=False)
         self.val_pred = output
+        self.val_target = target
         self.log("val_loss", loss)
-        # self.iou(pred.float(), target)
-        # self.log("val_iou", self.iou, on_step=False, on_epoch=True)
+        pred = output.argmax(dim=1, keepdim=False)
+        self.accuracy(pred, target)
+        self.log("val_acc", self.accuracy, on_step=False, on_epoch=True)
 
     def on_validation_epoch_end(self):
-        mask_image = mask.generate_mask_from_class_probabilities(
-            self.val_pred, self.num_classes
-        )
+        mask_image = generate_mask(self.val_pred, self.num_classes + 1)
+        target_mask = generate_mask(self.val_target, self.num_classes + 1, False)
 
         for logger in self.loggers:
             if isinstance(logger, TensorBoardLogger):
@@ -156,24 +119,11 @@ class UNetModule(pl.LightningModule):
 
             if isinstance(logger, WandbLogger):
                 logger.log_image(key="val_mask", images=[mask_image])
+                logger.log_image(key="val_target", images=[target_mask])
 
     def configure_optimizers(self):
-        optimizer = optim.SGD(
+        optimizer = optim.Adam(
             self.parameters(),
             lr=self.lr,
-            momentum=self.momentum,
-            weight_decay=self.weight_decay,
         )
-        # lr_scheduler = optim.lr_scheduler.OneCycleLR(
-        #     optimizer,
-        #     max_lr=self.lr,
-        #     epochs=self.trainer.max_epochs,
-        #     steps_per_epoch=len(self.trainer.datamodule.train_dataloader()),
-        # )
-        # return [optimizer], [
-        #     {
-        #         "scheduler": lr_scheduler,
-        #         "interval": "step",
-        #     },
-        # ]
         return optimizer
