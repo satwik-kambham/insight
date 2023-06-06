354832a623c147ebd9768d33393bc5e5103a2b89
Satwik Kambham <satwik.kambham@gmail.com>
2023-06-06 20:01:30 +0530

    l2 regularization

--- insight/classification/train/train.py
+++ insight/classification/train/train.py
@@ -100,7 +100,7 @@ def train(
 @click.option("--num_workers", type=int, default=4, help="Number of workers")
 @click.option("--lr", type=float, default=0.001, help="Learning rate")
 @click.option("--momentum", type=float, default=0.9, help="Momentum")
-@click.option("--weight_decay", type=float, default=0.0, help="Weight decay")
+@click.option("--weight_decay", type=float, default=0.0001, help="Weight decay")
 @click.option("--epochs", type=int, default=20, help="Number of epochs")
 @click.option(
     "--accelerator", type=str, default="auto", help="Accelerator: auto, cpu, gpu, tpu"
